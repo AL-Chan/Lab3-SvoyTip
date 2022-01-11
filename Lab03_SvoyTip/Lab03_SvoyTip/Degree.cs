99e6d404258ccc208577cb5563257599f29482fb
Maxim Chan <90909658+AL-Chan@users.noreply.github.com>
2022-01-11 13:15:39 +0800

    Add files via upload

---
+++ Lab03_SvoyTip/Lab03_SvoyTip/Degree.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_SvoyTip
+{
+    // варианты градусов
+    public enum DegreeTip {C, F, Ra, K};
+
+    public class Degree
+    {
+        private double value;
+        private DegreeTip tip;
+
+        public Degree(double value, DegreeTip tip) // значение,  тип
+        {
+            this.value = value;
+            this.tip = tip;
+        }
+
+    // сложение
+        public static Degree operator +(Degree instance, double number)
+        {
+            var newValue = instance.value + number;
+            var degree = new Degree(newValue, instance.tip);
+            return degree;
+        }
+        public static Degree operator +(double number, Degree instance)
+        {
+            return instance + number;
+        }
+    // сложение двух величин
+        public static Degree operator +(Degree instance1, Degree instance2)
+        {
+            return instance1 + instance2.Vyvod(instance1.tip).value;
+        }
+
+    // вычитание
+        public static Degree operator -(Degree instance, double number)
+        {
+            var newValue = instance.value - number;
+            var degree = new Degree(newValue, instance.tip);
+            return degree;
+        }
+        public static Degree operator -(double number, Degree instance)
+        {
+            return instance - number;
+        }
+    // вычитание двух величин
+        public static Degree operator -(Degree instance1, Degree instance2)
+        {
+            return instance1 - instance2.Vyvod(instance1.tip).value;
+        }
+        
+    // умножение
+        public static Degree operator *(Degree instance, double number)
+        {
+            var newValue = instance.value * number;
+            var degree = new Degree(newValue, instance.tip);
+            return degree;
+        }
+        public static Degree operator *(double number, Degree instance)
+        {
+            return instance * number;
+        }
+
+    //сравнение
+        public static string Sravnenie(Degree instance1, Degree instance2) 
+        {
+            string outMessage = "";
+            var newValue1 = instance1.value;
+            var newValue2 = instance2.value;
+            if (newValue1 < newValue2)  // условие
+            {
+                outMessage = (newValue1 + " < " + newValue2);
+            }
+            else
+            {
+                outMessage = (newValue1 + " > " + instance2);
+            }
+            
+            //var degree = new Degree(newValue, instance.tip);
+            return outMessage;
+        }
+        public Degree Vyvod(DegreeTip newTip)
+       {
+            var newValue = this.value;
+            // если текущий тип Цельсий переводить в другие градусы
+            if (this.tip == DegreeTip.C)
+            {
+                switch (newTip)
+                {
+                    //конвертируем в градусы Цельсия
+                    case DegreeTip.C:
+                        newValue = this.value;
+                        break;
+                    //в градусы Фаренгейта
+                    case DegreeTip.F:
+                        newValue = this.value * 1.8 + 32;
+                        break;
+                    //в градусы Ранкина
+                    case DegreeTip.Ra:
+                        newValue = this.value * 1.8 + 491.67;
+                        break;
+                    //в Кельвины
+                    case DegreeTip.K:
+                        newValue = this.value + 273.15;
+                        break;
+                }
+            }
+            else if (newTip == DegreeTip.C) // перевод других градусов в Цельсий
+            {
+                switch(this.tip)
+                {
+                    case DegreeTip.C:
+                        newValue = this.value;
+                        break;
+                    case DegreeTip.F:
+                        newValue = (this.value - 32) * 5 / 9;
+                        break;
+                    case DegreeTip.Ra:
+                        newValue = (this.value - 491.67) * 5 / 9;
+                        break;
+                    case DegreeTip.K:
+                        newValue = this.value - 273.15;
+                        break;
+                }
+            }
+            else // для случаев когда другие градусы в другие градусы
+            {
+                newValue = this.Vyvod(DegreeTip.C).Vyvod(newTip).value;
+            }
+            return new Degree(newValue, newTip);
+       }
+       
+        public string Sort() //вывод значений
+        {
+            return String.Format("{0} {1}", this.value, this.tip);
+        }
+    }
+}
---
+++ Lab03_SvoyTip/Lab03_SvoyTipTests/DegreeTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab03_SvoyTip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_SvoyTip.Tests
+{
+    [TestClass()]
+    public class DegreeTests
+    {
+        [TestMethod()]
+        public void SortTest() //вывод величин
+        {
+            var degree = new Degree(36.6, DegreeTip.C);
+            Assert.AreEqual("36,6 C", degree.Sort());
+
+            degree = new Degree(36.6, DegreeTip.F);
+            Assert.AreEqual("36,6 F", degree.Sort());
+
+            degree = new Degree(36.6, DegreeTip.Ra);
+            Assert.AreEqual("36,6 Ra", degree.Sort());
+
+            degree = new Degree(36.6, DegreeTip.K);
+            Assert.AreEqual("36,6 K", degree.Sort());
+        }
+
+        [TestMethod()]
+        public void SlozhenieTest() //сложение
+        {
+            var degree = new Degree(36.6, DegreeTip.C);
+            degree = degree + 10.5;
+            Assert.AreEqual("47,1 C", degree.Sort());
+        }
+
+        [TestMethod()]
+        public void VychitanieTest() //вычитание
+        {
+            var degree = new Degree(28.0, DegreeTip.Ra);
+            degree = degree - 16.8;
+            Assert.AreEqual("11,2 Ra", degree.Sort());
+        }
+        
+        [TestMethod()] //сложение и вычитание двух величин
+        public void DveRaznieVelichiny_Slozh_Vychet()
+        {
+            var c = new Degree(36, DegreeTip.C); //96.8 F
+            var f = new Degree(59, DegreeTip.F); //15.0 C
+
+            Assert.AreEqual("51 C", (c + f).Sort());
+            Assert.AreEqual("155,8 F", (f + c).Sort());
+
+            Assert.AreEqual("-37,8 F", (f - c).Sort());
+            Assert.AreEqual("21 C", (c - f).Sort());
+        }
+
+        [TestMethod()]
+        public void UmnojenieNaChicloTest() //умножение на число
+        {
+        var degree = new Degree(18.0, DegreeTip.F);
+        degree = degree * 3;
+        Assert.AreEqual("54 F", degree.Sort());
+        }
+
+        [TestMethod()]
+        public void CelcVDrugoe() //вывод значения цельсия в любом типе
+        {
+        Degree degree;
+
+        degree = new Degree(10, DegreeTip.C);
+        Assert.AreEqual("50 F", degree.Vyvod(DegreeTip.F).Sort());
+
+        degree = new Degree(20, DegreeTip.C);
+        Assert.AreEqual("527,67 Ra", degree.Vyvod(DegreeTip.Ra).Sort());
+
+        degree = new Degree(30, DegreeTip.C);
+        Assert.AreEqual("303,15 K", degree.Vyvod(DegreeTip.K).Sort());
+        }
+        
+        [TestMethod()]
+        public void DrugoeVCelc() //вывод любого другого типа в значения цельсия
+        {
+            Degree degree;
+
+            degree = new Degree(50, DegreeTip.F);
+            Assert.AreEqual("10 C", degree.Vyvod(DegreeTip.C).Sort());
+
+            degree = new Degree(671.67, DegreeTip.Ra);
+            Assert.AreEqual("100 C", degree.Vyvod(DegreeTip.C).Sort());
+
+            degree = new Degree(1, DegreeTip.K);
+            Assert.AreEqual("-272,15 C", degree.Vyvod(DegreeTip.C).Sort());
+        }
+
+    }
+}
